a234627b61eec3cb2d81b18f6d8f86fdbb53380d
AlinaSte <alina.job.g@gmail.com>
2023-03-27 11:07:06 +0300

    Homework nr 3

--- src/main/java/org/example/homework_nr_3/TemperatureConverter.java
+++ src/main/java/org/example/homework_nr_3/TemperatureConverter.java
@@ -2,12 +2,14 @@ package org.example.homework_nr_3;
 
 public class TemperatureConverter {
     public void toCelsius(double C) {
+        System.out.print(C+"F");
         C=(C-32)*5/9;
-        System.out.println("15F is celsius "+C);
+        System.out.println(" in celsius is "+C);
     }
     public void toFahrenheit(double F) {
+        System.out.print(F+"C");
         F=(F*9/5)+32;
-        System.out.println("3C is fahrenheit "+F);
+        System.out.println(" in fahrenheit is "+F);
     }
     public static void main(String[] args) {
         double Celsius=3;
